a1be91fa1d9d64cfc348ead3121417f550547ec2
Themaytrix <obengraymond81@gmail.com>
2024-11-08 22:30:39 -0700

    media-config

--- django-notes-app/noteapp/settings.py
+++ django-notes-app/noteapp/settings.py
@@ -139,6 +139,9 @@ STATICFILES_DIRS = [
     BASE_DIR/'frontend/build/static'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/ubuntu/notes/django-notes-app/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
